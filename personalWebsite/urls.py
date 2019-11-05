ef0269b95e132f64882f43edbaa46a819f6b4ba4
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-11-05 00:23:13 -0500

    finished

--- personalWebsite/urls.py
+++ personalWebsite/urls.py
@@ -1,4 +1,4 @@
-"""personalWebsite URL Configuration
+"""django_tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- polls/models.py
+++ polls/models.py
@@ -12,6 +12,11 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    #this allows the function to be used as a filter with was_published_recently
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
